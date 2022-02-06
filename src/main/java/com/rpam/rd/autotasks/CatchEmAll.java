3cb40e5f0993926bf4928c1342cdff80d60d644e
AnnaTsukiyo <annett.sav4enkko@gmail.com>
2022-02-06 12:53:41 +0200

    done

--- src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
+++ src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
@@ -4,28 +4,40 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class CatchEmAll {
-    static Exception exception = new FileNotFoundException();
-    static Exception exception2 = new IllegalArgumentException();
-    static Exception exception3 = new IOException();
+    //You may set another exception in this field;
+    static Exception exception = null;
 
-    public static void riskyMethod() throws IllegalArgumentException, IOException {
-        throw new IllegalArgumentException();
+    private static void riskyMethod() throws Exception {
+        if (exception != null) {
+            throw exception;
+        }
     }
-    //
-//Какое исключение брошено?	Как с ним обращаться?
-//    IOException	Обернуть в IllegalArgumentException с сообщением "Resource error" и выбросить
-//    FileNotFoundException	Обернуть в IllegalArgumentException с сообщением "Resource is missing" и выбросить
-//    ArithmeticException or NumberFormatException	Печатать сообщение о сгенерированном исключении в System.err. Ничего не генерировать
-//    Любое другое исключение	Не перехватывать
-    public static void main(String[] args) throws FileNotFoundException, IOException,IllegalArgumentException {
+
+    public static void main(String... args) throws Exception  {
+        if (args.length >= 1) {
+            if (args[0].equals("IOException")) {
+                exception = new IOException("IO exception");
+            } else if (args[0].equals("FileNotFoundException")) {
+                exception = new FileNotFoundException("file not found exception");
+            } else if (args[0].equals("ArithmeticException")) {
+                exception = new ArithmeticException("arithmetic exception");
+            } else if (args[0].equals("NumberFormatException")) {
+                exception = new NumberFormatException();
+            } else if (args[0].equals("Exception")) {
+                exception = new Exception("exception");
+            } else {
+                System.out.println("Используйте аргумент IOException, FileNotFoundException, ArithmeticException, NumberFormatException, Exception");
+            }
+        }
+
         try {
             riskyMethod();
-        } catch (FileNotFoundException exception1){
-            throw  new IllegalArgumentException("Resource is missing");
-        } catch (IOException exception){
-            throw new IllegalArgumentException("Resource error");
-        } catch (ArithmeticException | NumberFormatException exception3){
-            exception3.printStackTrace();
+        } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException("Resource is missing", e);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Resource error", e);
+        } catch (ArithmeticException| NumberFormatException ex) {
+            System.err.println(ex.getMessage());
         }
     }
 }
